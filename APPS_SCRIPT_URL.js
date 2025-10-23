788d2aa10fb23d57468c668f797d2de3cadff4fb
Arno1848 <arno.bowi@web.de>
2025-10-23 11:35:41 +0200

    Update APPS_SCRIPT_URL.js version and URLs

--- APPS_SCRIPT_URL.js
+++ APPS_SCRIPT_URL.js
@@ -1,8 +1,8 @@
-// APPS_SCRIPT_URL.js (25.09.2025)
+// APPS_SCRIPT_URL.js (23.10.2025)
 
 //Testversion:
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoGKR58bdWuOmPyKYjCJKL33g-EGz1HLoFXuBZmaQccsbo_NHjU691n0Xu_h-DiU8tGA/exec";
 
 
 //Tafelversion:
-//const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxrlHyMIaNi1W3yQfC4Z24TTJFEZVyJG20V4fSddX0jxU5dYVb5r3J8GZh0H3LLytqB/exec";
+//const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzIb0TUV9vtEJvalGclDhnOGxnF8l8Li9xqat5koZYXyEvEvQKoqWeoDi_fTX4nTA4Ryw/exec";
